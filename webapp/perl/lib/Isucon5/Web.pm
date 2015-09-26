9940835376f95fdd0c8fcb64595ebf835ef3e18e
Ichinose Shogo <shogo82148@gmail.com>
2015-09-26 18:53:32 +0900

    こっちにもひつようだった

--- webapp/perl/lib/Isucon5/Web.pm
+++ webapp/perl/lib/Isucon5/Web.pm
@@ -500,7 +500,7 @@ FROM footprints_cache f
 JOIN users u ON u.id = f.owner_id
 WHERE user_id = ?
 GROUP BY created_date, user_id, owner_id
-ORDER BY created_date, updated DESC
+ORDER BY created_date DESC, updated DESC
 LIMIT 50
 SQL
     my $footprints = db->select_all($query, current_user()->{id});
